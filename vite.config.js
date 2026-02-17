43a403e006d13dc9a0f9aaff67a14285280df08b
Akmaltong <40340558+akmaltong@users.noreply.github.com>
2026-02-17 22:36:06 +0300

    fix: set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default {
     root: 'sources/', // Sources files (typically where index.html is)
     envDir: '../',  // Directory where the env file is located
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-    base: './', // Public path (what's after the domain)
+    base: '/mff-3d-navigation/', // GitHub Pages base path
     server:
     {
         // https: true,
